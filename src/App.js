f19981399272d5cfbb8cc46064d3104c4e84c978
Shuhayb Miah <shuhaybmiah@shuhaybs-mbp.home>
2022-04-10 01:45:38 +0100

    SEC-5 Adding routing and nav bar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
-import Directory from "./components/Directory";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./pages/navigation";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route path="/" element={<HomePage />} exact />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
