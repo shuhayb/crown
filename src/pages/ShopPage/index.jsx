c0c5bb24f08490d6e877fe3fe06b5c2a2284b156
Shuhayb Miah <shuhaybmiah@shuhaybs-mbp.home>
2022-04-16 03:50:45 +0100

    SEC-9 Adding Product Card component

--- src/pages/ShopPage/index.jsx
+++ src/pages/ShopPage/index.jsx
@@ -1,15 +1,13 @@
 import { useContext } from "react";
 import { ProductContext } from "../../context/products.context";
+import ProductCard from "../../components/ProductCard";
+import "./styles.scss";
 const ShopPage = () => {
   const { products } = useContext(ProductContext);
   return (
-    <div>
-      {products.map(({ id, name }) => {
-        return (
-          <div key={id}>
-            <h1>{name}</h1>
-          </div>
-        );
+    <div className="products-container">
+      {products.map((product) => {
+        return <ProductCard key={product.id} product={product} />;
       })}
     </div>
   );
